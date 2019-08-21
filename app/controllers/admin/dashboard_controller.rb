b04f30f6bbba75d8bc494fe9290dd994dd28cf27
vagrant <tk95@live.ca>
2019-08-21 22:35:21 +0000

    removed unnecessary string converter

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
   
 
-  http_basic_authenticate_with name: ENV['USERNAME'].to_s, password: ENV['PASSWORD'].to_s
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
     @product_count = Product.count
