a52da22319255261c18202dcb9088fc103a1adfd
vagrant <tk95@live.ca>
2019-08-23 17:08:10 +0000

    Added user authentication by creating users table, model and controller and a session controller to keep track of user logged in

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
   
 
-  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  http_basic_http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
     @product_count = Product.count
