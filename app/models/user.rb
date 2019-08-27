1a6863b685f660f84e975f1a90d71ef3126232bb
vagrant <tk95@live.ca>
2019-08-27 17:33:34 +0000

    Added user class method authenticate_with_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,23 @@
 class User < ActiveRecord::Base
 
     has_secure_password
+
+    validates_presence_of :first_name, 
+    :last_name,
+    :password,
+    :password_confirmation,
+    :email
+
+    validates :password, length: { minimum: 5}
+    validates :email, uniqueness: {:case_sensitive => false}
+
+    def self.authenticate_with_credentials(email, password)
+        user = User.find_by(email: email.downcase.strip).authenticate(password)
+        if (user)
+            user
+        else 
+            nil
+        end
+    end
+
 end
