e0c0412640e4d90625d352172f8df93da516a421
vagrant <tk95@live.ca>
2019-08-28 20:43:39 +0000

    Corrected typo in admin > catergories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
 
-    http_basic_http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+    http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
     def index
         @categories = Category.order(id: :desc).all
